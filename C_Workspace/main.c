8253e69037bddcb04933a722936b80b96a92de5f
Peter He <hexinpeter@gmail.com>
2014-09-01 20:32:01 +1000

    Initial Commit

---
+++ C_Workspace/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  C_Workspace
+//
+//  Created by He Xin on 1/09/2014.
+//  Copyright (c) 2014 He Xin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
